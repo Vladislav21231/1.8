6b4f9c657fe57b18a4ca892437dcfb7212a965c3
DELL <vladislav.solo2017@yandex.ru>
2022-11-30 13:19:08 +0300

    commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -4,6 +4,12 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+public class User package hiber.model;
+
+        import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
    @Id
@@ -19,8 +25,12 @@ public class User {
    @Column(name = "email")
    private String email;
 
-   public User() {}
-   
+   @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+   private Car car;
+
+   public User() {
+   }
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
@@ -57,5 +67,24 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public Car setCar(Car car) {
+      this.car = car;
+      return car;
+   }
+
+   @Override
+   public String toString() {
+      return "User {" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              '}';
    }
 }
